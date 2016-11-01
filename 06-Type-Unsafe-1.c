26874562409f016c1922b7fd5a2f41d0c3822126
domfarolino <domfarolino@gmail.com>
2016-11-01 11:43:10 -0400

    Updated memcpy example

--- 06-Type-Unsafe-1.c
+++ 06-Type-Unsafe-1.c
@@ -13,21 +13,21 @@ typedef struct { char arr[100]; } S1;
 typedef struct { char arr[10]; } S2;
 
 int main() {
-   int i;
-   /* reverse the order of the next two lines to reverse the order in
-      which the data for these variables occurs on the stack */
+  int i;
+  /* reverse the order of the next two lines to reverse the order in
+    which the data for these variables occurs on the stack */
 
-   S2 b;
-   S1 a;
+  S2 b;
+  S1 a;
 
-   strncpy(a.arr,"Joe is my best friend and he is from Sandusky\0",36);
-   strncpy(b.arr,"Pete\0",5);
+  strncpy(a.arr, "Joe is my best friend and he is from Sandusky\0", 36);
+  strncpy(b.arr, "Pete\0", 5);
 
-   printf("a.arr (before) = %s\n", a.arr);
-   printf("b.arr (before) = %s\n", b.arr);
+  printf("b.arr (before) = %s\n", b.arr);
+  printf("a.arr (before) = %s\n", a.arr);
 
-   memcpy(b.arr, a.arr, 36);
+  memcpy(b.arr, a.arr, 36);
 
-   printf("a.arr (after)  = %s\n", a.arr);
-   printf("b.arr (after)  = %s\n", b.arr);
+  printf("b.arr (after)  = %s\n", b.arr);
+  printf("a.arr (after)  = %s\n", a.arr);
 }
